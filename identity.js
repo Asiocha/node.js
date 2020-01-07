c5e659e1b9df5f84b28d318e0889c9408264f548
Joanna Jasik <jasik.joanna87@gmail.com>
2020-01-07 21:20:00 +0100

    Add genders

--- identity.js
+++ identity.js
@@ -1,24 +1,39 @@
 const fs = require('fs');
 
 const genders = ['male', 'female'];
-const maleNames = ['Krystyna', 'Paulina', 'Grażyna', 'Halina'];
-const femaleNames = ['Krzysztof', 'Roman', 'Piotr', 'Rafał'];
+const femaleNames = ['Krystyna', 'Paulina', 'Grażyna', 'Halina'];
+const maleNames = ['Krzysztof', 'Roman', 'Piotr', 'Rafał'];
 const lastNames = ['Zimoch', 'Krzak', 'Groch', 'Paluch'];
 const age = ['21', '33', '68', '71'];
 
 const arr = [genders, maleNames, femaleNames, lastNames, age]
 
-function randChoice = () => {
+
+
+const randChoice = arr => {
    return arr[Math.floor(arr.length * Math.random())];
 };
 
 const people = [];
 
-for(let i = 0; i < 20; i++){
-    
+for (let i = 0; i < 20; i++) {
+   let person = {};
+   person.gender = randChoice(genders);
+   if (person.gender === "male") {
+      person.name = randChoice(maleNames);
+
+   }
+   else {
+      person.name = randChoice(femaleNames);
+   }
+   person.lastName = randChoice(lastNames);
+   person.age = randChoice(age);
+   people.push(person);
 }
 
-fs.writeFile('outputfile.txt', data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+const json = JSON.stringify(people);
+
+fs.writeFile('people.json', json, (err) => {
+   if (err) throw err;
+   console.log('The json has been saved!');
+});
